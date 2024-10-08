e8cf63c1b01bfca06a432e45c432dea4426ef353
d3tourrr <wrhoades94@gmail.com>
2024-10-08 07:34:45 -0700

    fix windows helper script

--- start-windows-companion.ps1
+++ start-windows-companion.ps1
@@ -1,12 +1,12 @@
 # Change the value in quotation marks to change the default name for your companion
 # You may want to do this if you're running multiple instances of this bot
-# Ex: $companionName = "friend_1"
-$defaultName = "discord_companion"
+# Ex: $companionName = $defaultName = "friend_1"
+$companionName = "discord_companion"
 
 ### DO NOT EDIT BELOW THIS LINE ###
-$inputName = Read-Host "Companion Name (name of the Docker container) is set to $defaultName - is this okay? Press Enter to accept this name or enter another one"
+$inputName = Read-Host "Companion Name (name of the Docker container) is set to $companionName - is this okay? Press Enter to accept this name or enter another one"
 
-if ([string]::IsNullOrWhiteSpace($inputName)) {$companionName = $defaultName}
+if (-not [string]::IsNullOrWhiteSpace($inputName)) {$companionName = $inputName}
 
 docker container rm $companionName -f
 docker build -t $companionName $psscriptroot
