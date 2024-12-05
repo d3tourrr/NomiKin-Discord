789c4b6b642892bf378eb78fe23bf096274fa506
d3tourrr <wrhoades94@gmail.com>
2024-12-05 07:20:55 -0800

    fix helper scripts

--- start-linux-companion.sh
+++ start-linux-companion.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# Change the value in quotation marks to change the default name for your companion
-# You may want to do this if you're running multiple instances of this bot
-# Ex: companionName = "friend_1"
-containerName="nomikin-discord"
+containerName="NomiKinDiscord"
 read -p "Docker container name is set to $containerName - is this okay? Press Enter to accept this name or enter another one: " inputName
 
 if [ -n "$inputName" ]; then
@@ -13,5 +10,5 @@ fi
 scriptroot="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 docker container rm $containerName -f
 docker build -t $containerName $scriptroot
-docker run -d --name $containerName
-echo "Run `docker logs --tail 50 $containerName` to ensure setup was successful"
+docker run -d --name $containerName $containerName
+echo "Run \`docker logs --tail 50 $containerName\` to ensure setup was successful"
--- start-windows-companion.ps1
+++ start-windows-companion.ps1
@@ -1,14 +1,11 @@
-# Change the value in quotation marks to change the default name for your companion
-# You may want to do this if you're running multiple instances of this bot
-# Ex: $companionName = $defaultName = "friend_1"
-$containerName = "discord_companion"
-
-### DO NOT EDIT BELOW THIS LINE ###
+$containerName = "NomiKinDiscord"
 $inputName = Read-Host "Docker container name is set to $containerName - is this okay? Press Enter to accept this name or enter another one"
 
-if (-not [string]::IsNullOrWhiteSpace($inputName)) {$containerName = $inputName}
+if (-not [string]::IsNullOrWhiteSpace($inputName)) {
+    $containerName = $inputName
+}
 
 docker container rm $containerName -f
-docker build -t $containerName $psscriptroot
-docker run -d --name $containerName
-Write-Output "Run `docker logs --tail 50 $containerName to check to ensure setup was successful"
+docker build -t $containerName $PSScriptRoot
+docker run -d --name $containerName $containerName
+Write-Output "Run `docker logs --tail 50 $containerName` to check and ensure setup was successful"
